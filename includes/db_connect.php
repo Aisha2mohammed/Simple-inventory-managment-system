cf0a8bbb038e416336df4af69d7b803cb8e38886
Aisha2mohammed <hayunadus111@gmail.com>
2025-01-20 09:40:14 +0300

    DB connection place

---
+++ includes/db_connect.php
@@ -0,0 +1,16 @@
+
+<?php
+$host = 'localhost';
+$db_name = 'uims'; // Replace with your actual database name
+$username = 'root'; // Default XAMPP username
+$password = ''; // Default XAMPP password
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
+
+
